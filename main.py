2e28a99e54417c71366b5da8653be7d662830c6d
jtejolan <thrills1.leis@icloud.com>
2026-07-06 21:33:20 -0400

    Fist commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "My API is live"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.get("/hello/{name}")
+def hello(name: str):
+    return {"message": f"Hello, {name}"}
