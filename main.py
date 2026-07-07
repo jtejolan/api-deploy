df2bbd93f08e4132af312e9cdb6cb90e99703540
jtejolan <thrills1.leis@icloud.com>
2026-07-06 22:09:33 -0400

    Items added

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = [
+    {"id": 1, "name": "Projector", "status": "available"},
+    {"id": 2, "name": "Chess Set", "status": "missing pieces"},
+    {"id": 3, "name": "Button Maker", "status": "available"},
+    {"id": 4, "name": "Ninja Creami", "status": "new"}
+
+]
+
 @app.get("/")
 def home():
     return {"message": "My API is live"}
@@ -17,3 +25,15 @@ def hello(name: str):
 @app.get("/new")
 def new():
     return {"message": f"This is a new endpoint."}
+
+@app.get("/items")
+def get_items():
+    return items
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int):
+    for item in items:
+        if item[id] == item_id:
+            return item
+    
+    return {"error": "Item not found"}
